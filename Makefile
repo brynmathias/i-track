dc6c58e4d98bc6d813c04fea3c444aa4c4387f9f
Bryn Mathias <brynmathias@gmail.com>
2012-08-06 14:21:14 +0100

    pull in arlo's changes

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-CC = clang++
+CC = g++-4.7
+CFLAGS = -std=c++11
 LDFLAGS = -I/usr/local/include/opencv -lm -lopencv_core -lopencv_highgui -lopencv_video -lopencv_imgproc
 ALL = test
 
--- test.cpp
+++ test.cpp
@@ -3,23 +3,28 @@
 
 using namespace cv;
 
+
+cv::Mat getImage(int capDev, cv::VideoCapture cap){
+  if(!cap.isOpened()) assert "capture device";
+  cv::Mat image;
+  cap >> image;
+  return image;
+}
+
+
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
-
-    Mat edges;
+    cv::VideoCapture cap(0);
+    cv::Mat edges;
     namedWindow("edges",1);
     for(;;)
     {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+        cv::Mat frame { getImage(0,cap) };
+        cv::cvtColor(frame, edges, CV_BGR2GRAY);
+        cv::GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        cv::Canny(edges, edges, 0, 30, 3);
+        cv::imshow("edges", edges);
+        if(cv::waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
